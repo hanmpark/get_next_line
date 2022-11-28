72304deef4e08c2c3503f5bb4ce68f930bccb249
Hanmin Park <hanmpark@student.42nice.fr>
2022-11-28 14:42:59 +0100

    28/11/2022: segfault

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,48 @@
 /*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 13:55:49 by hanmpark          #+#    #+#             */
-/*   Updated: 2022/11/28 12:10:19 by hanmpark         ###   ########.fr       */
+/*   Updated: 2022/11/28 13:58:47 by hanmpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int	ft_countc(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (str[count] && str[count] != '\n')
+		count++;
+	return (count);
+}
+
+char	*ft_strncpy(char *s, char *buf, size_t len)
+{
+	while (*buf && len--)
+	{
+		*s = *buf;
+		buf++;
+	}
+	*s = '\0';
+	return (s);
+}
+
 char	*get_next_line(int fd)
 {
-	int		i;
+	int		len;
 	char	*s;
 	char	*buf;
 
 	if (!fd)
 		return (NULL);
-	read(fd, buf, );
+	read(fd, buf, BUFFER_SIZE);
+	len = ft_countc(buf);
+	s = malloc((len + 1) * sizeof(char));
+	if (!s)
+		return (NULL);
+	s = ft_strncpy(s, buf, len);
+	return (s);
 }
 
 #include <stdio.h>
