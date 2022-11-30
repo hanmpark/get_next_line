a45352e69220ff55b8b1a6b10bb67245043a91cf
Hanmin Park <hanmpark@student.42nice.fr>
2022-11-30 19:14:58 +0100

    30/11/2022: removed the comment

--- get_next_line.c
+++ get_next_line.c
@@ -6,30 +6,13 @@
 /*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 13:55:49 by hanmpark          #+#    #+#             */
-/*   Updated: 2022/11/30 19:08:59 by hanmpark         ###   ########.fr       */
+/*   Updated: 2022/11/30 19:14:17 by hanmpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 #include <string.h>
-
-/*
-** Steps to follow:
-** 1. read the file and put to buf
-** 2. s[len] len = -> \n
-** 3. find a way to keep the chars after the '\n' and read at the start of this
-**
-** txt.txt
-**	Hello World
-**	How are you today
-**
-** if (BUFFER_SIZE == 13)
-** buf = "Hello World\nH"
-** len = 11
-** buf = "\nH" -> maybe put that in the struct
-** buf = "\nHow are you to"
-*/
 
 int	check_n(char *str)
 {
